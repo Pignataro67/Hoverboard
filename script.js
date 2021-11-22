711675064611e4ba07e67d5b83dd6e4d685655c9
Pignataro67 <Pignataro67@gmail.com>
2021-11-22 05:02:07 -0800

    adding JS

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function setColor(element) {
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
+function removeColor(element) {
+  element.style.background = '#1d1d1d'
+  element.style.boxShadow = '0 0 2px #000'
+}
+
 function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)]
 }
