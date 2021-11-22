f8f5cd8fb5388ce1763bb9f3ef8ca9ed6a8ab84b
Pignataro67 <Pignataro67@gmail.com>
2021-11-22 05:01:28 -0800

    adding JS

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
 function setColor(element) {
   const color = getRandomColor()
   element.style.background = color
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
 }
